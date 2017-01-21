f5d4e54a5bd813c5cd08315d0e4ba52c4c71f061
RubenTil <rubenvantil@gmail.com>
2017-01-21 18:15:24 +0100

    Adding base activity

--- app/src/main/java/nl/hsleiden/notifier/BaseActivity.java
+++ app/src/main/java/nl/hsleiden/notifier/BaseActivity.java
@@ -1,13 +1,116 @@
 package nl.hsleiden.notifier;
 
+import android.support.annotation.LayoutRes;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.support.design.widget.NavigationView;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.FrameLayout;
+import android.widget.Toast;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 
 public class BaseActivity extends AppCompatActivity {
+
+
+    @BindView(R.id.toolbar)
+    Toolbar toolbar;
+
+
+    @BindView(R.id.navigation_view)
+    NavigationView navigationView;
+
+
+    @BindView(R.id.drawer)
+    DrawerLayout drawerLayout;
+
+    @BindView(R.id.view_stub)
+    FrameLayout view_stub;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_base);
+        super.setContentView(R.layout.activity_base);
+        ButterKnife.bind(this);
+
+        setSupportActionBar(toolbar);
+
+
+        //Setting Navigation View Item Selected Listener to handle the item click of the navigation menu
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+
+            // This method will trigger on item Click of navigation menu
+            @Override
+            public boolean onNavigationItemSelected(MenuItem menuItem) {
+
+
+                //Checking if the item is in checked state or not, if not make it in checked state
+                if(menuItem.isChecked()) menuItem.setChecked(false);
+                else menuItem.setChecked(true);
+
+                //Closing drawer on item click
+                drawerLayout.closeDrawers();
+
+                //Check to see which item was being clicked and perform appropriate action
+                switch (menuItem.getItemId()){
+
+
+                    //Replacing the main content with ContentFragment Which is our Inbox View;
+                    case R.id.nav_overview:
+                        Toast.makeText(getApplicationContext(),"Overview Selected",Toast.LENGTH_SHORT).show();
+                        return true;
+
+                    // For rest of the options we just show a toast on click
+
+                    case R.id.nav_timeline:
+                        Toast.makeText(getApplicationContext(),"Timeline Selected",Toast.LENGTH_SHORT).show();
+                        return true;
+                }
+            return false;
+            }
+        });
+
+        // Initializing Drawer Layout and ActionBarToggle
+        ActionBarDrawerToggle actionBarDrawerToggle = new ActionBarDrawerToggle(this,drawerLayout,toolbar,R.string.openDrawer, R.string.closeDrawer);
+
+        //Setting the actionbarToggle to drawer layout
+        drawerLayout.addDrawerListener(actionBarDrawerToggle);
+
+        //calling sync state is necessay or else your hamburger icon wont show up
+        actionBarDrawerToggle.syncState();
+
+
+    }
+    @Override
+    public void setContentView(@LayoutRes int layoutResID) {
+        if (view_stub != null) {
+            LayoutInflater inflater = (LayoutInflater) getSystemService(LAYOUT_INFLATER_SERVICE);
+            ViewGroup.LayoutParams lp = new ViewGroup.LayoutParams(
+                    ViewGroup.LayoutParams.MATCH_PARENT,
+                    ViewGroup.LayoutParams.MATCH_PARENT);
+            View stubView = inflater.inflate(layoutResID, view_stub, false);
+            view_stub.addView(stubView, lp);
+        }
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (drawerLayout.isDrawerOpen(Gravity.LEFT)) {
+            drawerLayout.closeDrawer(Gravity.LEFT);
+        } else {
+            super.onBackPressed();
+        }
     }
 }
+
